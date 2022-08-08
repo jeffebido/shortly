0d1dc44f68982399ac1ac549f42375a4b59dcc7e
Jefferson Oliveira <jeffebido@hotmail.com>
2022-08-08 16:33:37 -0300

    Env exemplo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import authRouter from './routes/authRouter.js';
 import urlsRouter from './routes/urlsRouter.js';
 import usersRouter from './routes/usersRouter.js';
 import rankingRouter from './routes/rankingRouter.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -18,6 +21,6 @@ app.use(usersRouter);
 app.use(rankingRouter);
 
 
-app.listen(5000, '127.0.0.1', () => {
-    console.log('Server is listening on port 5000.');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}.`);
 });
